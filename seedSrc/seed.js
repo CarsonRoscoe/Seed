790ddbca585c35f439b6e7e603631cdf2ed92585
Carson Roscoe <CarsonRoscoe7@gmail.com>
2018-01-11 00:55:30 -0800

    Created a prototype of an executible virtual machine

--- seedSrc/seed.js
+++ seedSrc/seed.js
@@ -5,6 +5,37 @@ const cryptographyExporter = require("./cryptography.js");
 const cryptographyHelper = cryptographyExporter.newCryptographyHelper();
 const cryptographyUnitTests = cryptographyExporter.newCryptographyUnitTests();
 
-cryptographyUnitTests.RunTests();
+//cryptographyUnitTests.RunTests();
 
 console.log("Test Complete");
+
+const vmExporter = require("./virtualMachine.js");
+
+let svm = vmExporter.createVirtualMachine();
+let mathModule = vmExporter.createModule({module : "math", version : "1.0.0"});
+let multiply = {
+    function : "multiply",
+    version : "1.0.0",
+    invoke : function (a, b) {
+        return a * b;
+    }
+}
+
+let leanMultiplyHash = cryptographyHelper.SHA256(multiply.function + multiply.version);
+let fullMultiplyHash = cryptographyHelper.SHA256(multiply.invoke.toString() + multiply.function + multiply.version);
+
+mathModule.addFunction(multiply);
+
+svm.addModule(mathModule);
+
+let multiply2 = svm.getFunction({module : "math", function : "multiply", version : "1.0.0"});
+
+let leanInfoValidation = svm.isFunctionLeanInfoCorrect({ module : "math", function : "multiply", version : "1.0.0"}, leanMultiplyHash);
+let fullValidation = svm.isFunctionCorrect({module : "math", function : multiply.function, version : multiply.version}, fullMultiplyHash);
+
+if (leanInfoValidation && fullValidation) {
+    let answer = multiply.invoke(5, 10);
+    console.log("math.multipl version 1.0 says 5*10=" + answer);
+} else {
+    console.log("Failed: " + leanInfoValidation + " " + fullValidation);
+}
